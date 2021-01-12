6c62ef3cbea7cd85c9cd18b7ab622161f864ff3f
SR3u <wnmsr3u@gmail.com>
2021-01-12 23:52:09 +0300

    Minor fixes

--- src/test/java/sr3u/jvec/benchmark/DefaultMathMatrixBenchmark.java
+++ src/test/java/sr3u/jvec/benchmark/DefaultMathMatrixBenchmark.java
@@ -1,25 +1,31 @@
 package sr3u.jvec.benchmark;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import sr3u.jvec.JMath;
 import sr3u.jvec.Matrix;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
 
 /*
-*
-* Results on my machine (Java implementation, AMD Ryzen 7 3800X, 8 cores 16 threads, 3.9 GHz, boost to 4.2 GHz):
-*
-*    @Test add 1.39 ms
-*    @Test sub 1.74 ms
-*    @Test hadamardProduct 1.317 ms
-*    @Test multiply 39.955 ms
-*    @Test transpose 0.001 ms
-*
-*/
+ *
+ * Results on my machine (Java implementation, AMD Ryzen 7 3800X, 8 cores 16 threads, 3.9 GHz, boost to 4.2 GHz):
+ *
+ *    @BeforeClass machineInfo
+ *      CPU: amd64 cores (threads): 16
+ *      OS: Windows 10 version 10.0
+ *      Java: 15.0.1 Oracle Corporation
+ *    @Test add 1.39 ms
+ *    @Test sub 1.74 ms
+ *    @Test hadamardProduct 1.317 ms
+ *    @Test multiply 39.955 ms
+ *    @Test transpose 0.001 ms
+ *
+ */
 
 @Ignore
 public class DefaultMathMatrixBenchmark {
@@ -28,6 +34,14 @@ public class DefaultMathMatrixBenchmark {
     private static final JMath math = JMath.get();
 
     private final Random random = new Random();
+
+    @BeforeClass
+    public static void machineInfo() throws ClassNotFoundException {
+        System.out.println(getMethodName(BeforeClass.class));
+        System.out.println("\tCPU: " + System.getProperty("os.arch") + " cores (threads): " + Runtime.getRuntime().availableProcessors());
+        System.out.println("\tOS: " + System.getProperty("os.name") + " version " + System.getProperty("os.version"));
+        System.out.println("\tJava: " + System.getProperty("java.version") + " " + System.getProperty("java.vendor"));
+    }
 
     @Test
     public void transpose() {
@@ -78,14 +92,18 @@ public class DefaultMathMatrixBenchmark {
         System.out.println(methodName + " " + timeMs + " ms");
     }
 
-    private String getMethodName() throws ClassNotFoundException {
+    private static String getMethodName() throws ClassNotFoundException {
+        return getMethodName(Test.class);
+    }
+
+    private static String getMethodName(final Class<? extends Annotation> annotationClass) throws ClassNotFoundException {
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
         for (StackTraceElement e : stackTrace) {
             Class<?> aClass = Class.forName(e.getClassName());
             if (Arrays.stream(aClass.getDeclaredMethods())
                     .filter(m -> m.getName().equals(e.getMethodName()))
-                    .anyMatch(m -> m.getAnnotation(Test.class) != null)) {
-                return "@" + Test.class.getSimpleName() + " " + e.getMethodName();
+                    .anyMatch(m -> m.getAnnotation(annotationClass) != null)) {
+                return "@" + annotationClass.getSimpleName() + " " + e.getMethodName();
             }
         }
         return "<unknown>";
